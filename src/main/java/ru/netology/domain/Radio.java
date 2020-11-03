12af574cdec878302643eadbdc7f978c4ac94f39
Veronazavr <verochka10@list.ru>
2020-11-04 00:28:38 +0300

    added updated Lombok

--- src/main/java/ru/netology/domain/Radio.java
+++ src/main/java/ru/netology/domain/Radio.java
@@ -1,5 +1,12 @@
 package ru.netology.domain;
 
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+
 public class Radio {
     private int maxStation = 9; // Максмимальная станция
     private int minStation = 0; // Минимальная станция
@@ -8,139 +15,5 @@ public class Radio {
     private int minVolume = 0; // Минимальная громкость
     private int currentVolume; // Выбранная громкость
 
-    public Radio(int maxStation,
-                 int minStation,
-                 int currentStation,
-                 int maxVolume,
-                 int minVolume,
-                 int currentVolume) {
-        this.maxStation = maxStation;
-        this.minStation = minStation;
-        this.currentStation = currentStation;
-        this.maxVolume = maxVolume;
-        this.minVolume = minVolume;
-        this.currentVolume = currentVolume;
-    }
 
-    public Radio(int MaxStation, int minStation, int currentStation) {
-    }
-
-    public Radio(int maxStation, int minStation) {
-    }
-
-    public Radio(int maxStation) {
-    }
-
-    public Radio() {
-
-    }
-
-//    станции
-
-    public int getMaxStation() {
-        return maxStation;
-    }
-
-    public void setMaxStation(int maxStation) {
-        this.maxStation = maxStation;
-    }
-
-    public int getMinStation() {
-        return minStation;
-    }
-
-    public void setMinStation(int minStation) {
-        this.minStation = minStation;
-    }
-
-    public int getCurrentStation() {
-        return currentStation;
-    }
-
-    public void setCurrentStation(int currentStation) {
-        if (currentStation > maxStation) {
-            return;
-        }
-        if (currentStation < minStation) {
-            return;
-        }
-        this.currentStation = currentStation;
-    }
-
-    public void nextCurrentStation() {
-        int currentStation = this.currentStation;
-        if (currentStation >= maxStation) {
-            this.currentStation = minStation;
-        } else {
-            this.currentStation = currentStation + 1;
-        }
-    }
-
-    public void prevCurrentStation() {
-        int currentStation = this.currentStation;
-        if (currentStation <= minStation) {
-            this.currentStation = maxStation;
-        } else {
-            this.currentStation = currentStation - 1;
-        }
-    }
-
-    public void remoteCurrentStation(int currentStation) {
-        if (currentStation > maxStation) {
-            this.currentStation = maxStation;
-        } else if (currentStation < minStation) {
-            this.currentStation = minStation;
-        } else {
-            this.currentStation = currentStation;
-        }
-    }
-
-    // громкость
-    public int getMaxVolume() {
-        return maxVolume;
-    }
-
-    public void setMaxVolume(int maxVolume) {
-        this.maxVolume = maxVolume;
-    }
-
-    public int getMinVolume() {
-        return minVolume;
-    }
-
-    public void setMinVolume(int minVolume) {
-        this.minVolume = minVolume;
-    }
-
-    public int getCurrentVolume() {
-        return currentVolume;
-    }
-
-    public void setCurrentVolume(int currentVolume) {
-        if (currentVolume > maxVolume) {
-            return;
-        }
-        if (currentVolume < minVolume) {
-            return;
-        }
-        this.currentVolume = currentVolume;
-    }
-
-    public void nextCurrentVolume() {
-        int currentVolume = this.currentVolume;
-        if (currentVolume >= maxVolume) {
-            this.currentVolume = maxVolume;
-        } else {
-            this.currentVolume = currentVolume + 1;
-        }
-    }
-
-    public void prevCurrentVolume() {
-        int currentVolume = this.currentVolume;
-        if (currentVolume <= minVolume) {
-            this.currentVolume = minVolume;
-        } else {
-            this.currentVolume = currentVolume - 1;
-        }
-    }
 }
