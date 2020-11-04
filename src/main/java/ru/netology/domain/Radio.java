8f6590ae52d485da48ae03b167cb544a74e41517
Veronazavr <verochka10@list.ru>
2020-11-04 14:35:35 +0300

    added updated branch

--- src/main/java/ru/netology/domain/Radio.java
+++ src/main/java/ru/netology/domain/Radio.java
@@ -8,64 +8,27 @@ public class Radio {
     private int minVolume = 0; // Минимальная громкость
     private int currentVolume; // Выбранная громкость
 
-    public Radio(int maxStation,
-                 int minStation,
-                 int currentStation,
-                 int maxVolume,
-                 int minVolume,
-                 int currentVolume) {
+    public Radio (int maxStation,
+                  int minStation,
+                  int currentStation){
         this.maxStation = maxStation;
         this.minStation = minStation;
         this.currentStation = currentStation;
-        this.maxVolume = maxVolume;
-        this.minVolume = minVolume;
-        this.currentVolume = currentVolume;
-    }
-
-    public Radio(int MaxStation, int minStation, int currentStation) {
-    }
-
-    public Radio(int maxStation, int minStation) {
-    }
-
-    public Radio(int maxStation) {
-    }
-
-    public Radio() {
 
     }
-
+//    public Radio (int maxStation,
+//                  int minStation,
+//                  int currentStation,
+//                  int maxVolume,
+//                  int minVolume,
+//                  int currentVolume) {
+//        this.maxStation = maxStation;
+//        this.minStation = minStation;
+//        this.currentStation = currentStation;
+//        this.maxVolume = maxVolume;
+//        this.minVolume = minVolume;
+//        this.currentVolume = currentVolume;
 //    станции
-
-    public int getMaxStation() {
-        return maxStation;
-    }
-
-    public void setMaxStation(int maxStation) {
-        this.maxStation = maxStation;
-    }
-
-    public int getMinStation() {
-        return minStation;
-    }
-
-    public void setMinStation(int minStation) {
-        this.minStation = minStation;
-    }
-
-    public int getCurrentStation() {
-        return currentStation;
-    }
-
-    public void setCurrentStation(int currentStation) {
-        if (currentStation > maxStation) {
-            return;
-        }
-        if (currentStation < minStation) {
-            return;
-        }
-        this.currentStation = currentStation;
-    }
 
     public void nextCurrentStation() {
         int currentStation = this.currentStation;
@@ -95,36 +58,68 @@ public class Radio {
         }
     }
 
+    public int getMaxStation() {
+        return maxStation;
+    }
+
+//    public void setMaxStation(int maxStation) {
+//        this.maxStation = maxStation;
+//    }
+
+    public int getMinStation() {
+        return minStation;
+    }
+
+//    public void setMinStation(int minStation) {
+//        this.minStation = minStation;
+//    }
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+//    public void setCurrentStation(int currentStation) {
+//        if (currentStation > maxStation) {
+//            return;
+//        }
+//        if (currentStation < minStation) {
+//            return;
+//        }
+//        this.currentStation = currentStation;
+//    }
+
+
+
     // громкость
     public int getMaxVolume() {
         return maxVolume;
     }
 
-    public void setMaxVolume(int maxVolume) {
-        this.maxVolume = maxVolume;
-    }
+//    public void setMaxVolume(int maxVolume) {
+//        this.maxVolume = maxVolume;
+//    }
 
     public int getMinVolume() {
         return minVolume;
     }
 
-    public void setMinVolume(int minVolume) {
-        this.minVolume = minVolume;
-    }
+//    public void setMinVolume(int minVolume) {
+//        this.minVolume = minVolume;
+//    }
 
     public int getCurrentVolume() {
         return currentVolume;
     }
 
-    public void setCurrentVolume(int currentVolume) {
-        if (currentVolume > maxVolume) {
-            return;
-        }
-        if (currentVolume < minVolume) {
-            return;
-        }
-        this.currentVolume = currentVolume;
-    }
+//    public void setCurrentVolume(int currentVolume) {
+//        if (currentVolume > maxVolume) {
+//            return;
+//        }
+//        if (currentVolume < minVolume) {
+//            return;
+//        }
+//        this.currentVolume = currentVolume;
+//    }
 
     public void nextCurrentVolume() {
         int currentVolume = this.currentVolume;
